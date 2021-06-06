c1c0fb3a2907bb34f1754bc105f9063d94f05ad4
Andres Felipe Ballesteros Gonzalez <a.ballesteros@globant.com>
2021-06-06 10:22:56 -0500

    error 1 reminder

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,19 +2,9 @@ import React from 'react';
 
 const ErrorExample = () => {
   let random = 'Random text'
-  let handleClick = () => {
-    console.log('Random title')
-  }
-  return (
-    <React.Fragment>
-      <h2>
-        {random}
-      </h2>
-      <button type="button" className="btn" onClick={handleClick}>
-        Change title
-      </button>
-    </React.Fragment>
-  );
-};
+  return <React.Fragment>
+    <h2>{random}</h2>
+  </React.Fragment>
+}
 
 export default ErrorExample;
