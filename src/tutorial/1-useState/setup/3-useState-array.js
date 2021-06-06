4b54b275386dbc71dcba521f0077dd2665995141
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-06 12:30:59 -0500

    First way to handle the useState function

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -4,16 +4,20 @@ import { data } from '../../../data'
 const UseStateArray = () => {
   // Second way to use the hook useState with React.useState
   const [people, setPeople] = React.useState(data)
+  const handle = () => {
+    setPeople([])
+  }
   return <>
     {people.map((element) => {
       //Destructuring the element (every single object inside the data array) by "id" and "name"
       const {id, name} = element
       return (
-        <div key={id} className='item'>
+        <div className='item'>
           <h4>{name}</h4>
         </div>
       )
     })}
+    <button className='btn' onClick={handle}>Clear me</button>
 
   </>
 };
