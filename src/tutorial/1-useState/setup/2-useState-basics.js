b0c0751b948a29f3336a4d4612f050f31419fe9f
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-06 10:58:01 -0500

    2 useState fundamental returns an array with a value and a function to handle that value

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,20 +1,11 @@
 import React, { useState } from 'react';
+// useState is a function that came from React
 
 const UseStateBasics = () => {
-  const [text, setText] = useState('Hello Ultrasoft!')
-  const handleClick = () => {
-    setText("Hello React World");
-  }
+  console.log(useState('Ultrasoft'));
   return (
-    <React.Fragment>
-      <h2>{text}</h2>
-      <button className='btn' onClick={handleClick}>Click me</button>
-    </React.Fragment>
+    <h2>Hello Ultra!</h2>
   );
-  
-  
-  
-
 };
 
 export default UseStateBasics;
