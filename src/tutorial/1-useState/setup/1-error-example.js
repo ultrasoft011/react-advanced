5edc85d5b2ad3247f38801e7e5d1ac862e188503
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-06 10:49:34 -0500

    error 1 useState starting

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -4,7 +4,9 @@ const ErrorExample = () => {
   let random = 'Random text'
   //handleClick function: to handle the button declared on the JSX "btn"
   const handleClick = () => {
-    console.log(`${"It's working"}`)
+    //The handleClick would not render the new 'random' variable on the VirtualDOM, it is not rerenders the value - useState will allow to do that
+    random = 'Im changing the text'
+    console.log(`${"It's working!"}`)
   }
   return <React.Fragment>
     <h2>{random}</h2>
