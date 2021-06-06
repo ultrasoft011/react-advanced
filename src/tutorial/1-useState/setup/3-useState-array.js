269a1666e8fe8dc2c4a2ee55a11ac53c706cf01b
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-06 12:21:01 -0500

    3 useState second way to invoke it and using map with destructuring to access to the data info

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { data } from '../../../data';
+import { data } from '../../../data'
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // Second way to use the hook useState with React.useState
+  const [people, setPeople] = React.useState(data)
+  return <>
+    {people.map((element) => {
+      //Destructuring the element (every single object inside the data array) by "id" and "name"
+      const {id, name} = element
+      return (
+        <div key={id} className='item'>
+          <h4>{name}</h4>
+        </div>
+      )
+    })}
+
+  </>
 };
 
 export default UseStateArray;
