92c51da1a73e9cd324f26a9ef8f50cb696ae790b
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-06 10:42:25 -0500

    error 1 handle the button reminder

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const ErrorExample = () => {
   let random = 'Random text'
+  //handleClick function: to handle the button declared on the JSX "btn"
+  const handleClick = () => {
+    console.log(`${"It's working"}`)
+  }
   return <React.Fragment>
     <h2>{random}</h2>
+    <button className="btn" onClick={handleClick}>Press me!</button>
   </React.Fragment>
 }
 
