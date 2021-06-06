bef098da898aa3afe4f05f112e068591287249d6
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-06 11:09:51 -0500

    2 useState destructuring to use the useState hook

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 // useState is a function that came from React
 
 const UseStateBasics = () => {
-  console.log(useState('Ultrasoft'));
+  console.log(useState());
+  // useState will return an array with a value and a function to handle that value
+  // const firstArg = useState('Ultra')[0];
+  // const secondArg = useState(1)[1];
+  // console.log(firstArg, secondArg);
+
+  // Destructuring to use the useState, define a const variable with [text, setText] could be name whatever I want and initialize to useState 
+  const [text, setText] = useState('Example');
+
   return (
-    <h2>Hello Ultra!</h2>
+    <React.Fragment>
+      <h1>{text}</h1>
+    </React.Fragment>
+
   );
 };
 
