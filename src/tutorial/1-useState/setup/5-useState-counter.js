c67b3a793f913f5031afdaf5e1c910f05569f81b
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-07 16:50:46 -0500

    useState Counter exercise

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [element, setElement] = useState('0')
+  return <>
+      <h2>App Counter</h2>
+      <h3>{element}</h3>
+      <button className='btn'>Increase</button>
+      <button className='btn'>Reset</button>
+      <button className='btn'>Decrease</button>
+    </>
 };
 
 export default UseStateCounter;
