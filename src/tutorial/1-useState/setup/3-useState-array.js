7caf1fa2778f39abe9c1ae4a4ccb2cac442282c3
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-22 10:45:56 -0500

    complex increase - functional update form

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import { data } from '../../../data'
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
   // Second way to use the hook useState with React.useState
-  const [people, setPeople] = React.useState(data)
+  const [people, setPeople] = React.useState(data);
   // First way to handle the useState function second argument
   // const handle = () => {
   //   setPeople([])
   // }
   const handleRemove = (id) => {
     // Filter with an specific condition to return only the items different than the id that was clicked
-    let newPeople = people.filter((element => element.id !== id));
-    setPeople(newPeople);    
-  }
-  return <>
-    {people.map((element) => {
-      //Destructuring the element (every single object inside the data array) by "id" and "name"
-      const {id, name} = element
-      return (
-        <div className='item' key={id}>
-          <h4>{name}</h4>
-          <button className='btn' onClick={() => handleRemove(id)}>Remove</button>
-        </div>
-      )
-    })}
-    {/* Second way to handle the useState function */}
-    <button className='btn' onClick={() => setPeople([])}>Clear me</button>
-  </>
+    let newPeople = people.filter((element) => element.id !== id);
+    setPeople(newPeople);
+  };
+  return (
+    <>
+      {people.map((element) => {
+        //Destructuring the element (every single object inside the data array) by "id" and "name"
+        const {id, name} = element;
+        return (
+          <div className="item" key={id}>
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => handleRemove(id)}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      {/* Second way to handle the useState function */}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear me
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  const [element, setElement] = useState(0)
-  return <>
+  // Use the useState hook, and pass a '0' as argument
+  const [element, setElement] = useState(0);
+  const handleReset = () => {
+    setElement(0);
+  };
+  const ComplexIncrease = () => {
+    setTimeout(() => {
+      {/* pass as argument a prevState or previos state to let Javascript know about the previous value that it should take into account */}
+      setElement((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+  return (
+    <>
       <h2>App Counter</h2>
       <h3>{element}</h3>
-      <button className='btn' onClick={() => setElement(element + 1)}>Increase</button>
-      <button className='btn'>Reset</button>
+      {/* Handle the increase and decrease buttons, I can set up the button function directly*/}
+      <button className="btn" onClick={() => setElement(element + 1)}>
+        Increase
+      </button>
+      <button className='btn' onClick={handleReset}>Reset</button>
       <button className='btn' onClick={() => setElement(element - 1)}>Decrease</button>
+
+      <h2>Complex Counter</h2>
+      <h3>{element}</h3>
+      <button className='btn' onClick={ComplexIncrease}>Increase with delay</button>
     </>
+  );
 };
 
 export default UseStateCounter;
