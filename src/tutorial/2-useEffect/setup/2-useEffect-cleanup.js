b7b925421fb3269321ec1fe4ca46b2ce07a6744e
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-23 12:42:14 -0500

    useEffect fundamentals

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,24 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  // useState hook
+  // window.innerWidth: will bring the actual width of a window
+  const [size, setSize] = useState(window.innerWidth);
+
+  // useEffect hook: to change the width as soon as the window get resized
+  // EventListener with the 'resize' event to listen the changes
+  // checkSize: will be the function to handle the new width information
+
+  const checkSize = () => {
+    // Call the setSize from the useState hook and pass as argument the variable (window.innerWidth), that will be the new value of the window width and the {size} will recognize the new value
+    setSize(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+  });
+  return <>
+    <h1>{size}</h1>
+  </>;
 };
 
 export default UseEffectCleanup;
