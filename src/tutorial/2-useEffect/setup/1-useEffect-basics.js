fd72ef37e8297fa83d247ad6e2fb803448b8882a
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-23 11:50:20 -0500

    useEffect hook fundamentals

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,18 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  // useState: value and setValue to increase de "value"
+  const [value, setValue] = useState(0);
+  // useEffect: by default it will rerender whatever is inside in the callback function
+  useEffect(() => {
+    console.log('Pressed!')
+  })
+  return (
+  <div>
+    <h2>{value}</h2>
+    <button className='btn' onClick={() => setValue(value + 1)}>Press me to increase</button>
+  </div>
+  )
 };
 
 export default UseEffectBasics;
