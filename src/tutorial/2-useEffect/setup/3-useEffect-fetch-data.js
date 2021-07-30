35490d9d8ee324efb7c029421b12c5448f189932
ultrasoft011 <ultrasoft.011@gmail.com>
2021-07-29 21:29:26 -0500

    multiple returns

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const url = "https://api.github.com/users";
+const url = "https://api.jikan.moe/v3/anime/16498/episodes?rapidapi-key=a2ec0e9e25msh160d9a5d4920085p16266djsnda83215d52a0";
 
 const UseEffectFetchData = () => {
   // 1. Set up the useState hook
@@ -10,22 +10,33 @@ const UseEffectFetchData = () => {
     // Set up the variable that will fecth the data
     const response = await fetch(url);
     // Set up the variable that will save the data as json format
+    console.log(response)
     const users = await response.json();
     // Update the "value" -> render
-    // console.log(users);
+    console.log(users.episodes);
     setValue(users);
   };
 
   // 2. Set up the useEffect hook -> Careful with the dependency, use [] as second parameter to prevent inifite loop
   useEffect(() => {
-    getData();
+    getData().then(data => setValue(data));
   }, []);
   
-  return <>{users.map((user) => {
-    return <>
-    <h1>{user.login}</h1>
-    </>
-  })}</>;
-};
+  return (
+    <ul>
+      {users.episodes.map((item) => (
+        <li key={item.episode_id}>{item.title}</li>
+      ))}
+    </ul>
+  );
+      }
+
+      {/* {users.map((user) => {
+        return (
+          <>
+            <h1>{user.episode_id}</h1>
+          </>
+        );
+      })} */}
 
 export default UseEffectFetchData;
